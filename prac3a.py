c7de3ea774a44134eb8866830c77d07ec403ddb4
KhanAmin7 <aminkhan2494.ak7@gmail.com>
2023-09-14 19:55:51 +0530

    prac 3a completed

---
+++ prac3a.py
@@ -0,0 +1,12 @@
+stack=[]
+stack.append('AMIT')
+stack.append('BHAKTI')
+stack.append('KARAN')
+print(f"Initial stack: {stack}")
+# Popping elements from the top of a list
+print('\nelement popped from the stack:')
+print(stack.pop())
+print(stack.pop())
+
+print("\nstack after element popped")
+print(stack)
